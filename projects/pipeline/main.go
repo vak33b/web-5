8e6184f6557b574c3215f0cf70293603f5790d4f
Alexander Suvorov <vak33beats@gmail.com>
2024-11-18 22:17:46 +0300

    work, calculator, pipeline and pdf file was added

--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,34 @@
 package main
 
-// реализовать removeDuplicates(in, out chan string)
+import "fmt"
+
+func removeDuplicates(in, out chan string) {
+	var a string
+	for i := range in {
+		if i != a {
+			out <- i
+			a = i
+		}
+	}
+	close(out)
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+	inputChan := make(chan string)
+	outputChan := make(chan string)
+	go func() {
+		inputChan <- "hello"
+		inputChan <- "world"
+		inputChan <- "hello"
+		inputChan <- "hello"
+		inputChan <- "test"
+		inputChan <- "alexander"
+		inputChan <- "hello"
+		inputChan <- "alex!"
+		close(inputChan)
+	}()
+	go removeDuplicates(inputChan, outputChan)
+	for s := range outputChan {
+		fmt.Println(s)
+	}
 }
